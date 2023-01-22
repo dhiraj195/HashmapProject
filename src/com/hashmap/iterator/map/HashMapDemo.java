aed03b0e9c0d64ab551e0e6a323925f1f7f77678
dhiraj <varpemahesh12@gmail.com>
2023-01-22 16:10:16 +0530

    HashMap Project Starts

---
+++ src/com/hashmap/iterator/map/HashMapDemo.java
@@ -0,0 +1,23 @@
+package com.hashmap.iterator.map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("Ram", "Patil");
+		map.put("Ajay", "Deshmukh");
+		
+		Iterator<Map.Entry<String, String>> itr = map.entrySet().iterator();
+		
+		while (itr.hasNext()) {
+			Map.Entry<String, String> entry = itr.next();
+			System.out.println("Key ="+ entry.getKey() +", Value = "+ entry.getValue());
+		}
+
+	}
+
+}
